7b1206996cd32613ba6b0797ced96cbf02401118
Scott O'Brien <scott@scottyob.com>
2024-10-28 13:35:23 -0700

    Got taskwarrior integration working

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
-use std::io;
+use std::collections::HashMap;
 
 use color_eyre::Result;
-use std::process::Command;
 
+use ansi_to_tui::IntoText;
 use ratatui::{
     buffer::Buffer,
     crossterm::event::{self, Event, KeyCode, KeyEventKind},
     layout::{Constraint, Layout, Rect},
     style::{palette::tailwind, Color, Stylize},
-    symbols,
-    text::{Line, ToText, Text},
     widgets::{Block, Padding, Paragraph, Tabs, Widget},
     DefaultTerminal,
 };
 use strum::{Display, EnumIter, FromRepr, IntoEnumIterator};
-use ansi_to_tui::IntoText;
 
+use strum_macros;
+// bring the trait into scope
+use strum::EnumProperty;
+
+mod taskwarrior;
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -29,6 +31,7 @@ fn main() -> Result<()> {
 struct App {
     state: AppState,
     selected_tab: SelectedTab,
+    reports: HashMap<SelectedTab, String>,
 }
 
 #[derive(Default, Clone, Copy, PartialEq, Eq)]
@@ -38,21 +41,35 @@ enum AppState {
     Quitting,
 }
 
-#[derive(Default, Clone, Copy, Display, FromRepr, EnumIter)]
+#[derive(
+    Default,
+    Clone,
+    Copy,
+    Display,
+    FromRepr,
+    EnumIter,
+    strum_macros::EnumProperty,
+    PartialEq,
+    Eq,
+    Hash,
+)]
 enum SelectedTab {
     #[default]
-    #[strum(to_string = "Tab 1")]
-    Tab1,
-    #[strum(to_string = "Tab 2")]
+    #[strum(to_string = "Due", props(cmd = "project.not:Bethany due"))]
+    Due,
+    #[strum(
+        to_string = "Active",
+        props(cmd = "project.not:Bethany active", Color = "false")
+    )]
     Tab2,
-    #[strum(to_string = "Tab 3")]
+    #[strum(to_string = "Inbox", props(cmd = "-PROJECT"))]
     Tab3,
-    #[strum(to_string = "Tab 4")]
-    Tab4,
 }
 
 impl App {
     fn run(mut self, mut terminal: DefaultTerminal) -> Result<()> {
+        self.reload_reports();
+
         while self.state == AppState::Running {
             terminal.draw(|frame| frame.render_widget(&self, frame.area()))?;
             self.handle_events()?;
@@ -72,6 +89,23 @@ impl App {
             }
         }
         Ok(())
+    }
+
+    fn reload_reports(&mut self) {
+        // Will run each report and store the result
+        for tab in SelectedTab::iter() {
+            let cmd = tab.get_str("cmd").expect("Enum expected command");
+            let color_str = tab.get_str("Color").unwrap_or("true");
+            let mut color = true;
+            if color_str == "false" {
+                color = false;
+            }
+
+            let output = taskwarrior::run(color, String::from(cmd).split(' '));
+            let output = output.expect("Expected TaskWarrior cmd to have a result");
+
+            self.reports.insert(tab, output);
+        }
     }
 
     pub fn next_tab(&mut self) {
@@ -106,131 +140,45 @@ impl SelectedTab {
 impl Widget for &App {
     fn render(self, area: Rect, buf: &mut Buffer) {
         use Constraint::{Length, Min};
-        let vertical = Layout::vertical([Length(1), Min(0), Length(1)]);
-        let [header_area, inner_area, footer_area] = vertical.areas(area);
+        let vertical = Layout::vertical([Length(1), Min(0)]);
+        let [header_area, inner_area] = vertical.areas(area);
 
-        let horizontal = Layout::horizontal([Min(0), Length(20)]);
-        let [tabs_area, title_area] = horizontal.areas(header_area);
+        let horizontal = Layout::horizontal([Min(0)]);
+        let [tabs_area] = horizontal.areas(header_area);
 
-        render_title(title_area, buf);
         self.render_tabs(tabs_area, buf);
-        self.selected_tab.render(inner_area, buf);
-        render_footer(footer_area, buf);
+
+        // Get the output for the tab
+        let tab_output = self
+            .reports
+            .get(&self.selected_tab)
+            .expect("Cmd result expected");
+        let text = tab_output.into_text().unwrap();
+        Paragraph::new(text)
+            .block(Block::new().padding(Padding::uniform(1)))
+            .render(inner_area, buf);
     }
 }
 
 impl App {
     fn render_tabs(&self, area: Rect, buf: &mut Buffer) {
-        let titles = SelectedTab::iter().map(SelectedTab::title);
-        let highlight_style = (Color::default(), self.selected_tab.palette().c400);
+
+        let titles = SelectedTab::iter().map(|t| {
+            let tab_output = self.reports.get(&t).expect("Cmd result expected");
+
+            format!(" {} ({}) ", t, taskwarrior::task_count(tab_output))
+                .fg(tailwind::SLATE.c600)
+                .bg(Color::default())
+        });
+
+        // let titles = SelectedTab::iter().map(SelectedTab::title);
+        let highlight_style = (Color::default(), Color::default());
         let selected_tab_index = self.selected_tab as usize;
         Tabs::new(titles)
             .highlight_style(highlight_style)
             .select(selected_tab_index)
             .padding("", "")
-            .divider(" | ")
+            .divider(" ")
             .render(area, buf);
-    }
-}
-
-fn render_title(area: Rect, buf: &mut Buffer) {
-    "Ratatui Tabs Example".bold().render(area, buf);
-}
-
-fn render_footer(area: Rect, buf: &mut Buffer) {
-    Line::raw("◄ ► to change tab | Press q to quit")
-        .centered()
-        .render(area, buf);
-}
-
-impl Widget for SelectedTab {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        // in a real app these might be separate widgets
-        match self {
-            Self::Tab1 => self.render_tab0(area, buf),
-            Self::Tab2 => self.render_tab1(area, buf),
-            Self::Tab3 => self.render_tab2(area, buf),
-            Self::Tab4 => self.render_tab3(area, buf),
-        }
-    }
-}
-
-impl SelectedTab {
-    /// Return tab's name as a styled `Line`
-    fn title(self) -> Line<'static> {
-        format!("  {self}  ")
-            .fg(tailwind::SLATE.c200)
-            .bg(self.palette().c100)
-            .into()
-    }
-
-    fn render_tab0(self, area: Rect, buf: &mut Buffer) {
-        // Capture the output of the TaskWarrior command
-        // std::thread::sleep(std::time::Duration::from_secs(1));
-
-        let output = Command::new("task")
-            .arg("rc.verbose:no")
-            .arg("rc._forcecolor:on")
-            .arg("rc.detection:off")
-            .arg("project.not:Bethany")
-            .arg("due")
-            .output()
-            .expect("Failed to execute command");
-
-        // Convert stdout to a String while preserving ANSI color codes
-        //let stdout = output.stdout.into_text().unwrap().trim(); //.unwrap();
-
-        let output_string= String::from_utf8_lossy(&output.stdout);
-        let output_string = output_string.trim();
-        let stdout = output_string.into_text().unwrap();
-
-        // let stdout= Text::from(output_string);
-        //let stdout = outstr.into_text().unwrap();
-
-
-        // Check and handle stderr output if necessary
-        let stderr = String::from_utf8_lossy(&output.stderr);
-        if !stderr.is_empty() {
-            eprintln!("Error output from task command: {}", stderr);
-        }
-
-        // Render the paragraph with ANSI codes intact
-        Paragraph::new(stdout).block(self.block()).render(area, buf);
-    }
-
-    fn render_tab1(self, area: Rect, buf: &mut Buffer) {
-        Paragraph::new("Welcome to the Ratatui tabs example!")
-            .block(self.block())
-            .render(area, buf);
-    }
-
-    fn render_tab2(self, area: Rect, buf: &mut Buffer) {
-        Paragraph::new("Look! I'm different than others!")
-            .block(self.block())
-            .render(area, buf);
-    }
-
-    fn render_tab3(self, area: Rect, buf: &mut Buffer) {
-        Paragraph::new("I know, these are some basic changes. But I think you got the main idea.")
-            .block(self.block())
-            .render(area, buf);
-    }
-
-    /// A block surrounding the tab's content
-    fn block(self) -> Block<'static> {
-        // Block::new()
-        Block::bordered()
-            .border_set(symbols::border::PROPORTIONAL_TALL)
-            .padding(Padding::horizontal(1))
-            .border_style(self.palette().c400)
-    }
-
-    const fn palette(self) -> tailwind::Palette {
-        match self {
-            Self::Tab1 => tailwind::BLUE,
-            Self::Tab2 => tailwind::EMERALD,
-            Self::Tab3 => tailwind::INDIGO,
-            Self::Tab4 => tailwind::RED,
-        }
     }
 }
---
+++ src/taskwarrior.rs
@@ -0,0 +1,24 @@
+use std::{ffi::OsStr, process::Command};
+
+pub fn run<I, S>(color: bool, args: I) -> Result<String, std::io::Error>
+where
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
+{
+    let mut cmd = Command::new("task");
+    cmd.arg("rc.verbose:no");
+    cmd.arg("rc._forcecolor:on");
+
+    if !color {
+        cmd.arg("rc.color.active=none");
+    }
+
+    let output = cmd.arg("rc.detection:off").args(args).output()?;
+
+    let output_string = String::from_utf8_lossy(&output.stdout);
+    Ok(String::from(output_string.trim()))
+}
+
+pub fn task_count(report: &String) -> usize {
+    return report.lines().count() - 2;
+}
